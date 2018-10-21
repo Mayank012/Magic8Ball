f5990967f039d8f607858846e802fa305c264b4b
Mayank012 <mayank.chauhan012@gmail.com>
2018-10-21 21:03:19 +0530

    Project Complete!

---
+++ app/src/main/java/com/example/mayank/magic_8_ball/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mayank.magic_8_ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = findViewById(R.id.image_8ball);
+
+        final int ballArray[] = new int[] {
+                R.drawable.ball1_2x,
+                R.drawable.ball2_2x,
+                R.drawable.ball3_2x,
+                R.drawable.ball4_2x,
+                R.drawable.ball5_2x,
+        };
+
+        Button myButton = findViewById(R.id.askButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            Random randomNumberGenerator = new Random();
+            int number = randomNumberGenerator.nextInt(5);
+            ballDisplay.setImageResource(ballArray[number]);
+
+            }
+        });
+    }
+}
+ 
